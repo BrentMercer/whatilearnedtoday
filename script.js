f850dbe154f134cf231411ea1001f00046855271
Brent Mercer <brentonjmercer@gmail.com>
2023-10-19 12:36:38 -0700

    Added API call to DB to populate facts.

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -49,9 +15,25 @@ const factsList = document.querySelector(".facts-list");
 
 factsList.innerHTML = "";
 
-fetch("https://jmobklayagsovcyskhfh.supabase.co");
+loadFacts();
 
-createFactsList(initialFacts);
+async function loadFacts() {
+  const res = await fetch(
+    "https://jmobklayagsovcyskhfh.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptb2JrbGF5YWdzb3ZjeXNraGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NzU1MDQsImV4cCI6MjAxMzE1MTUwNH0.iSi913yNv8RQYtBZG5VfbNWLx2dEzqVgzQBZ-5A2a8A",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptb2JrbGF5YWdzb3ZjeXNraGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc1NzU1MDQsImV4cCI6MjAxMzE1MTUwNH0.iSi913yNv8RQYtBZG5VfbNWLx2dEzqVgzQBZ-5A2a8A",
+      },
+    }
+  );
+  const data = await res.json();
+  //   const filteredData = data.filter((fact) => fact.category === "technology");
+  createFactsList(data);
+}
+
 function createFactsList(dataArray) {
   const htmlArr = dataArray.map(
     (fact) => `
@@ -63,7 +45,9 @@ function createFactsList(dataArray) {
                       target="_blank"
                       >(Source)</a
                   >
-                  <span class="tag" style="background-color: #3b82f6"
+                  <span class="tag" style="background-color: ${
+                    CATEGORIES.find((cat) => cat.name === fact.category).color
+                  }"
                       >${fact.category}</span
                   >
               </p>
